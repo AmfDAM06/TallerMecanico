c71de9be98cfacae8eb83b017567c63ae527eb11
Álvaro Martínez Flores <alvaromartinezflores06@gmail.com>
2025-05-20 18:33:42 +0200

    Commit inicial con el enunciado, las dependencias y los tests.

--- src/main/java/org/iesalandalus/programacion/tallermecanico/vista/ventanas/LanzadoraVentanaPrincipal.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/vista/ventanas/LanzadoraVentanaPrincipal.java
@@ -1,11 +1,38 @@
 package org.iesalandalus.programacion.tallermecanico.vista.ventanas;
 
-import org.iesalandalus.programacion.tallermecanico.Main;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import org.iesalandalus.programacion.tallermecanico.vista.ventanas.utilidades.Dialogos;
 
-public class LanzadoraVentanaPrincipal {
 
-    public static void main(String[] args) {
-        Main.main(args);
+public class LanzadoraVentanaPrincipal extends Application {
+
+    @Override
+    public void start(Stage escenarioPrincipal) {
+        try {
+            VBox raiz = FXMLLoader.load(LocalizadorVentanaPrincipal.class.getResource("vistas/ventanaPrincipal.fxml"));
+            Scene escena = new Scene(raiz);
+            escenarioPrincipal.setTitle("Taller Mecánico");
+            escenarioPrincipal.setScene(escena);
+            escenarioPrincipal.setOnCloseRequest(e->confirmarSalida(escenarioPrincipal,e));
+            escenarioPrincipal.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 
+    public static void comenzar(){
+        launch();
+    }
+
+    private void confirmarSalida(Stage escenarioPrincipal, WindowEvent e) {
+        if (Dialogos.mostrarDialogoConfirmacion("Salir de la aplicacion", "Estas seguro que quieres salir", escenarioPrincipal)) {
+            escenarioPrincipal.close();
+        } else
+            e.consume();
+    }
 }
